e9882536a2e300580cb3a19fd074820d6aef6145
Fog <2755236307@qq.com>
2023-09-20 22:53:49 +0800

    瑞吉快递初次创建

---
+++ src/main/java/cn/fog/ReggleDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.fog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReggleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReggleDemoApplication.class, args);
+    }
+
+}
